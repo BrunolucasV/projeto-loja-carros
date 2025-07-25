beec59d5ae3f12281f7d66dffe768f824cdfc531
Bruno Lucas <brunolucasa.g.v@gmail.com>
2025-07-24 21:53:09 -0300

    1.0.3
    Usando Pydantic no flask, para garantir que os campos estejam com informações corretas.

--- backend/rotas/carros.py
+++ backend/rotas/carros.py
@@ -1,9 +1,17 @@
 from flask import Blueprint, request, jsonify
+from pydantic import BaseModel, ValidationError
+from typing import List
+
 carros_bp = Blueprint("carros", __name__)
 
-# Simulando um banco com lista
+
 carros = []
 contador = 1
+
+class CarroModel(BaseModel):
+    marca: str
+    modelo: str
+    preco: float
 
 @carros_bp.route("/api/carros", methods=["GET"])
 def listar_carros():
@@ -12,13 +20,16 @@ def listar_carros():
 @carros_bp.route("/api/carros", methods=["POST"])
 def adicionar_carro():
     global contador
-    dados = request.json
-    novo = {
-        "id": contador,
-        "marca": dados["marca"],
-        "modelo": dados["modelo"],
-        "preco": dados["preco"]
-    }
-    carros.append(novo)
-    contador += 1
-    return jsonify({"mensagem": "Carro cadastrado com sucesso!"})
+    try:
+        dados = CarroModel(**request.json)
+        novo = {
+            "id": contador,
+            "marca": dados.marca,
+            "modelo": dados.modelo,
+            "preco": dados.preco
+        }
+        carros.append(novo)
+        contador += 1
+        return jsonify({"mensagem": "Carro cadastrado com sucesso!"})
+    except ValidationError as erro:
+        return jsonify({"erro": erro.errors()}), 400
